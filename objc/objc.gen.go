03bdf0d10fbb047042da88e94f0faa2a206d12d5
Travis Cline <travis.cline@gmail.com>
2026-03-18 14:29:16 -0700

    objc: regenerate objc

--- objc/objc.gen.go
+++ objc/objc.gen.go
@@ -53,6 +53,18 @@ func IDFrom(ptr unsafe.Pointer) ID {
 	return ID(uintptr(ptr))
 }
 
+// IDValueAt loads an Objective-C object ID stored at a symbol address.
+//
+// Dynamic libraries commonly export Objective-C object constants as pointers to
+// storage containing the real object ID. Dlsym returns the storage address, so
+// callers must load the pointer-sized value stored there.
+func IDValueAt(addr uintptr) ID {
+	if addr == 0 {
+		return 0
+	}
+	return *(*ID)(unsafe.Pointer(addr))
+}
+
 // NewBlock creates an Objective-C block from a Go function.
 // The Go function must take a Block as its first argument.
 // Use Block.Release() to free the block when it is no longer in use.
