c307e171f20f30cb398181130760d1c27a95f143
Travis Cline <travis.cline@gmail.com>
2026-03-18 18:35:21 -0700

    examples/metal4-triangle: add e2e test

---
+++ examples/metal/metal4-triangle/e2e.go
@@ -0,0 +1,6 @@
+package main
+
+// e2e.go provides the end-to-end test harness contract.
+//
+// Run with -e2e flag for automated non-interactive testing.
+// Exit codes: 0=pass/skip, 1=fail, 2=usage error.
