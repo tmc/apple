0d58bc1cd99d8af89c74dbf905171cb7c8035244
Travis Cline <travis.cline@gmail.com>
2026-05-15 19:45:31 -0700

    apple: add hypervisor framework

--- doc.go
+++ doc.go
@@ -26,6 +26,7 @@
 //go:generate applegen-bootstrap -f "endpointsecurity"
 //go:generate applegen-bootstrap -f "foundation"
 //go:generate applegen-bootstrap -f "gamecontroller"
+//go:generate applegen-bootstrap -f "hypervisor"
 //go:generate applegen-bootstrap -f "iokit"
 //go:generate applegen-bootstrap -f "iosurface"
 //go:generate applegen-bootstrap -f "kernel"
